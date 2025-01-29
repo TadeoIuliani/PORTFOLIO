62facd702dc9c150a1093834c0df2722a0e4e999
=TadeoIuliani <=tadeiuliani@gmail.com>
2025-01-29 19:08:27 -0300

    Pendiente agregarle mas diseño en bordes y funcionalidades

--- data/lenguajes.js
+++ data/lenguajes.js
@@ -1,19 +1,40 @@
 [
-    {
-      "img": "/images/js.png",
-      "descripcion": "JavaScript"
-    },
-    {
-      "img": "/images/nodejs.png",
-      "descripcion": "Nodejs"
-    },
-    {
-      "img": "/images/html-5.png",
-      "descripcion": "HTML"
-    },
-    {
-        "img": "/images/css-3.png",
-        "descripcion": "CSS"
-    }
+  {
+    "img": "/images/html-5.png",
+    "descripcion": "HTML"
+  },
+  {
+      "img": "/images/css-3.png",
+      "descripcion": "CSS"
+  },
+  {
+    "img": "/images/js.png",
+    "descripcion": "JavaScript"
+  },
+  {
+    "img": "/images/nodejs.png",
+    "descripcion": "Nodejs"
+  },
+  {
+      "img": "/images/base-de-datos.png",
+      "descripcion": "SQL"
+  },
+  {
+      "img": "/images/mysql.png",
+      "descripcion": "MYSQL"
+  },
+  {
+      "img": "/images/pyhton.png",
+      "descripcion": "Pyhton"
+  },
+  {
+      "img": "/images/C#.png",
+      "descripcion": "C#"
+  }
+  ,
+  {
+    "img": "/images/docker.png",
+    "descripcion": "Docker"
+  }
 ]
   
