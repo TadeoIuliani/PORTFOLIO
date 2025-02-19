adbcb4f08eef8b012019da5ee29e872be53e6f62
parcial <tadeiuliani@gmail.com>
2025-02-19 20:28:21 -0300

    Despligue de portfolio

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,7 +1,5 @@
 const fs = require("fs")
 
-/* aca lo utilizaremos para leer un archivo JSON que contiene informacion de  concesionarias */
-
 /* lectura del archivo JSON
 leemos el archivo consecionarias.json de forma síncrona */
 /* JSON.parse = convierte el contenido del archivo JSON en un objeto de JS para manipularlo mas facilmente en el codigo */
